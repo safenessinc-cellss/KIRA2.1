bfe0bc7b6b0b74387db0a7c09be47a4a65288eee
safenessinc-cellss <safeness.inc@gmail.com>
2026-07-02 10:17:59 -0300

    Add files via upload

--- server.ts
+++ server.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import { createServer as createViteServer } from "vite";
 import path from "path";
 import { fileURLToPath } from "url";
 import Stripe from "stripe";
@@ -218,6 +217,7 @@ async function startLocalServer() {
   
   // Vite integration
   if (process.env.NODE_ENV !== "production") {
+    const { createServer: createViteServer } = await import("vite");
     const vite = await createViteServer({
       server: { 
         middlewareMode: true,
